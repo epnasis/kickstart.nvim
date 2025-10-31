3e7193e2c16dd6e803a461e806194ef94322b391
Pawel Wenda <pawel@wenda.eu>
2025-10-31 23:33:04 +0100

    add mini plugin features

--- init.lua
+++ init.lua
@@ -871,6 +871,33 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      --
+      -- TODO: does not work - blink related?
+      -- Better navigation with Tab
+      --
+      local map_multistep = require('mini.keymap').map_multistep
+      map_multistep('i', '<Tab>', { 'pmenu_next' })
+      map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
+      map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
+      map_multistep('i', '<BS>', { 'minipairs_bs' })
+
+      --
+      -- ESCape input mode with jk keys
+      --
+      local map_combo = require('mini.keymap').map_combo
+
+      -- Support most common modes. This can also contain 't', but would
+      -- only mean to press `<Esc>` inside terminal.
+      local mode = { 'i', 'c', 'x', 's' }
+      map_combo(mode, 'jk', '<BS><BS><Esc>')
+
+      -- To not have to worry about the order of keys, also map "kj"
+      map_combo(mode, 'kj', '<BS><BS><Esc>')
+
+      -- Escape into Normal mode from Terminal mode
+      map_combo('t', 'jk', '<BS><BS><C-\\><C-n>')
+      map_combo('t', 'kj', '<BS><BS><C-\\><C-n>')
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
