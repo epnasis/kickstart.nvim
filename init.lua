d98c04a03c3e1fe3d290433646b1863e093ad2d7
Pawel Wenda <pawel@wenda.eu>
2025-11-29 03:50:51 +0100

    add bash type

--- init.lua
+++ init.lua
@@ -598,6 +598,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -642,6 +643,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'isort', -- Python import sorter
         'black', -- Python code formatter
+        'shfmt', -- Shell formatter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -696,6 +698,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        bash = { 'shfmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
