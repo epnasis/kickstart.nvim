052b7b68a7ea246868b490c9335044281d1a0634
Pawel Wenda <pawel@wenda.eu>
2025-12-22 00:49:19 +0100

    add snacks, refactor, fix errors - big one

--- lua/custom/plugins/venv-selector.lua
+++ lua/custom/plugins/venv-selector.lua
@@ -1,18 +1,19 @@
 return {
   'linux-cultist/venv-selector.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim' },
-  ft = 'python', -- Load when opening Python files
-  opts = {
-    -- A list of venv directory names to look for
-    name = { '.venv', 'venv' },
-    -- Command to create the virtual environment if none is found
-    search = {}, -- if you add your own searches, they go here.
-    options = {}, -- if you add plugin options, they go here.
+  branch = 'regexp', -- Use the new branch that doesn't depend on Telescope
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'folke/snacks.nvim',
   },
+  ft = 'python',
   keys = {
     { '<leader>pv', '<cmd>VenvSelect<cr>', desc = '[P]ython select [V]env' },
   },
-  config = function(_, opts)
-    require('venv-selector').setup(opts)
-  end,
+  opts = {
+    settings = {
+      options = {
+        notify_user_on_venv_activation = true,
+      },
+    },
+  },
 }
