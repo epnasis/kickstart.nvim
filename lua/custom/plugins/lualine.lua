adffd3f77bbca420c3095da30f0c6f6b9c40187b
Pawel Wenda <pawel@wenda.eu>
2025-12-22 01:11:41 +0100

    add lualine replacing mini statusline

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,35 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'catppuccin', -- Match your colorscheme
+      component_separators = '|',
+      section_separators = '',
+      globalstatus = true, -- One statusline for all windows (cleaner)
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        'branch',
+        {
+          'diff',
+          symbols = { added = ' ', modified = ' ', removed = ' ' }, -- Nice Nerd Font icons
+        },
+        'diagnostics',
+      },
+      lualine_c = { 'filename' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
+}
