b570cf011ca8e2032c93ca3a4eeee945c6ed09b7
Pawel Wenda <pawel@wenda.eu>
2025-12-22 01:02:44 +0100

    remove toggleterm - covered by snacks

--- lua/custom/plugins/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { 'akinsho/toggleterm.nvim', version = '*', config = true, keys = {
-    { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = '[T]erminal [T]oggle' },
-  } },
-  -- or
-  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
-}
