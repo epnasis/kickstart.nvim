1ea4bd9e497d2579f935672a0ee84aed42edf3b3
Pawel Wenda <pawel@wenda.eu>
2025-12-24 16:04:13 +0100

    change lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,33 +3,31 @@ return {
   dependencies = { 'echasnovski/mini.icons' },
   opts = {
     options = {
-      theme = 'catppuccin', -- Match your colorscheme
-      component_separators = '|',
-      section_separators = '',
-      globalstatus = true, -- One statusline for all windows (cleaner)
+      theme = 'catppuccin',
+      globalstatus = true,
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'snacks_dashboard' } },
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        winbar = 1000,
+        events = { 'WinEnter', 'BufEnter', 'BufWritePost', 'SessionLoadPost', 'FileChangedShellPost', 'VimResized', 'Filetype', 'CursorMoved', 'CursorMovedI', 'ModeChanged', 'TermClose', 'FocusGained' },
+      },
     },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = {
-        'branch',
+      lualine_b = { 'branch' },
+      lualine_c = {
         {
           'diff',
-          symbols = { added = ' ', modified = ' ', removed = ' ' }, -- Nice Nerd Font icons
+          symbols = { added = ' ', modified = ' ', removed = ' ' },
         },
         'diagnostics',
       },
-      lualine_c = { 'filename' },
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
-      lualine_y = {},
-      lualine_z = {},
     },
   },
 }
