6be803a21fbf60ee5e311db890ad8159ff7ce07c
Pawel Wenda <pawel@wenda.eu>
2026-01-02 00:37:38 +0100

    chore: init format

--- init.lua
+++ init.lua
@@ -814,7 +814,23 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'python', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'rust', 'toml', 'ron' },
+      ensure_installed = {
+        'python',
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'rust',
+        'toml',
+        'ron',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
